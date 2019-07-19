711fdf0e241689788f78a7202424bea0168ddafe
Kyle <kirvine87@gmail.com>
2019-07-19 15:17:55 +0100

    completed pets found and not found function

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,14 @@ end
 def stock_count(shop)
   return shop[:pets].size()
 end
+
+def pets_by_breed(shop, breed)
+  types = shop[:pets]
+  breed_array = []
+  for type in types
+    if type[:breed] == breed
+      breed_array.push(type)
+    end
+  end
+  return breed_array 
+end
